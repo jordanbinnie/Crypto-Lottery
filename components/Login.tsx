4695c875e26a4efc77c33ef56d900d1d42779a97
Jordan Binnie <jordanbinnie@Jordans-MacBook-Air.local>
2022-10-18 20:43:02 +1300

    updating

--- components/Login.tsx
+++ components/Login.tsx
@@ -6,7 +6,7 @@ function Login() {
     <div className="bg-[#091B18] min-h-screen flex flex-col items-center justify-center text-center">
         <div className="flex flex-col items-center mb-10">
             <h1 className="text-6xl text-white font-bold">The Crypto Lottery</h1>
-            <h2 className="text-white mt-5">Get started by logging in with your MetaMask</h2>
+            <h2 className="text-white mt-5">Get started by logging in with an existing wallet</h2>
 
             <ConnectWallet className="mt-10" />
         </div>
